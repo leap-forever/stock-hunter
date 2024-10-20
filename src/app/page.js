e3cb6a281433c8116a482912d270f5624fb19141
leap-forever <coderzc@gmail.com>
2024-10-20 23:19:14 +0800

    移除错误的 cookie 设置，不能在 page.js 上设置 cookie

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,6 @@
 import Image from "next/image";
-import { cookies } from 'next/headers'
-import { v4 as uuid_v4 } from "uuid";
 export default function Home() {
 
-  const cookieStore = cookies().set("id",uuid_v4())
-  console.log("cookie is",cookieStore)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       test
